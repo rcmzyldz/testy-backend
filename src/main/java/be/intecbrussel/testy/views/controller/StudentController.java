d920df7162c99d46917f0874dcd57b21fb9fb680
Yilmaz MUSTAFA <yilmaz@mail.be>
2021-08-13 00:34:08 +0200

    new ctrls are being created to continue working with student, teacher and manager endpoints.

---
+++ src/main/java/be/intecbrussel/testy/views/controller/StudentController.java
@@ -0,0 +1,13 @@
+package be.intecbrussel.testy.views.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/students")
+public class StudentController {
+
+
+
+
+}
